a89949e50f3682e4bd19b204894e7f6669f32cc5
mparker <mparker@pivotal.io>
2017-11-02 15:54:31 -0700

    first web test passing, cleaned up

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -2,32 +2,67 @@ const React = require("react")
 const ReactDOM = require("react-dom")
 
 class PlayForm extends React.Component {
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    handleSubmit(){
+        this.setState({message: "INVALID"})
+    }
+
     render(){
-        return <button>Hello World</button>
+        return <div>
+            {this.state.message}
+            <button onClick={this.handleSubmit.bind(this)}>PLAY</button>
+        </div>
     }
 }
 
 describe("play round form", function () {
     describe("when play determines the round is invalid", function () {
-        it("the user should see 'INVALID'", function () {
-            let domFixture = document.createElement("div")
-            domFixture.id = "hello"
-            document.body.appendChild(domFixture)
-
-            let rps = {
-                play(p1, p2, ui){
-                    ui.invalid()
-                }
-            }
-
-            ReactDOM.render(
-                <PlayForm rps={rps}/>,
-                domFixture
-            )
-
-            domFixture.querySelector("button").click()
-            expect(domFixture.innerText).toContain("INVALID")
+        beforeEach(function () {
+            renderForm({ play(p1, p2, ui){ ui.invalid() }})
         })
 
+        it("the user should see 'INVALID'", function () {
+            expect(page()).not.toContain("INVALID")
+            submitForm()
+            expect(page()).toContain("INVALID")
+        })
     })
+
+    let domFixture
+
+    beforeEach(function () {
+        setupDOM()
+    })
+
+    afterEach(function () {
+        cleanupDOM()
+    })
+
+    function cleanupDOM(){
+        domFixture.remove()
+    }
+    
+    function setupDOM() {
+        domFixture = document.createElement("div")
+        document.body.appendChild(domFixture)
+    }
+
+    function renderForm(rps) {
+        ReactDOM.render(
+            <PlayForm rps={rps}/>,
+            domFixture
+        )
+    }
+
+    function page() {
+        return domFixture.innerText;
+    }
+
+    function submitForm() {
+        domFixture.querySelector("button").click()
+    }
 })
