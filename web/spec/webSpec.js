08649755cf4599bef86bd3c595db5ecb8575fa19
mparker <mparker@pivotal.io>
2017-11-02 15:39:21 -0700

    failing web test

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,33 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+const React = require("react")
+const ReactDOM = require("react-dom")
+
+class PlayForm extends React.Component {
+    render(){
+        return <button>Hello World</button>
+    }
+}
+
+describe("play round form", function () {
+    describe("when play determines the round is invalid", function () {
+        it("the user should see 'INVALID'", function () {
+            let domFixture = document.createElement("div")
+            domFixture.id = "hello"
+            document.body.appendChild(domFixture)
+
+            let rps = {
+                play(p1, p2, ui){
+                    ui.invalid()
+                }
+            }
+
+            ReactDOM.render(
+                <PlayForm rps={rps}/>,
+                domFixture
+            )
+
+            domFixture.querySelector("button").click()
+            expect(domFixture.innerText).toContain("INVALID")
+        })
+
     })
 })
