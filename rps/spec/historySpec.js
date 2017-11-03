54a3120b837bda7d6a1c9672a03e7f3a576c82cf
mparker <mparker@pivotal.io>
2017-11-03 11:19:03 -0700

    Rps extracted

---
+++ rps/spec/historySpec.js
@@ -0,0 +1,8 @@
+describe("history", function () {
+    describe("no one has played", function () {
+        it("should tell the UI there are no rounds", function () {
+
+        })
+
+    })
+})
--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,41 +1,4 @@
-function Rps(){
-    this.playRound = function(p1Throw, p2Throw, ui){
-        new PlayRoundRequest(p1Throw, p2Throw, ui).process()
-    }
-}
-
-function PlayRoundRequest(p1Throw, p2Throw, ui){
-    this.process = function(){
-        if (invalid(p1Throw) || invalid(p2Throw))
-            ui.invalid()
-        else if (tie())
-            ui.tie()
-        else if (p1Wins())
-            ui.p1Wins()
-        else
-            ui.p2Wins()
-    }
-
-    const ROCK = "rock"
-    const PAPER = "paper"
-    const SCISSORS = "scissors"
-
-    const validThrows = [ROCK, PAPER, SCISSORS]
-
-    function invalid(t) {
-        return !validThrows.includes(t)
-    }
-
-    function tie() {
-        return p1Throw === p2Throw
-    }
-
-    function p1Wins() {
-        return p1Throw === ROCK     && p2Throw === SCISSORS ||
-               p1Throw === PAPER    && p2Throw === ROCK     ||
-               p1Throw === SCISSORS && p2Throw === PAPER
-    }
-}
+const Rps = require("../src/rps")
 
 describe("play Round", function () {
     let rps
