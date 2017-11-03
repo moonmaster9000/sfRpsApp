d093abc96fb099181c6f28250523df826a5d2f62
mparker <mparker@pivotal.io>
2017-11-03 12:24:23 -0700

    round repo contract passing

--- rps/spec/historySpec.js
+++ rps/spec/historySpec.js
@@ -12,7 +12,7 @@ describe("history", function () {
         })
     })
 
-    fdescribe("given rounds have been played", function () {
+    describe("given rounds have been played", function () {
         it("should send the round results to the UI", function () {
             let rps = new Rps()
             let historyUISpy = jasmine.createSpyObj("historyUISpy", ["rounds"])
@@ -34,3 +34,55 @@ describe("history", function () {
 
     })
 })
+
+function FakeRoundRepo(){
+    let rounds = []
+
+    this.isEmpty = function(){
+        return rounds.length === 0
+    }
+
+    this.save = function(round){
+        rounds.push(round)
+    }
+
+    this.getAll = function(){
+        return rounds
+    }
+
+}
+
+function roundRepoContract(roundRepo){
+    fdescribe("round repo contract", function () {
+        let repo
+
+        beforeEach(function () {
+            repo = new roundRepo()
+        })
+
+        describe("no rounds have been saved", function () {
+            it("is empty", function () {
+                expect(repo.isEmpty()).toBe(true)
+            })
+        })
+
+        describe("rounds have been saved", function () {
+            let round
+
+            beforeEach(function () {
+                round = new Round()
+                repo.save(round)
+            })
+
+            it("is not empty", function () {
+                expect(repo.isEmpty()).toBe(false)
+            })
+
+            it("returns all rounds saved", function () {
+                expect(repo.getAll()).toContain(round)
+            })
+        })
+    })
+}
+
+roundRepoContract(FakeRoundRepo)
